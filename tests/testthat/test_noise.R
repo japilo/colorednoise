4264b8417e88a91188fc910f1b3398aff14fcce6
Julia Pilowsky <japilo@gmail.com>
2017-10-08 22:45:30 +0200

    Renamed test files so they would run properly.

--- tests/testthat/test_noise.R
+++ tests/testthat/test_noise.R
@@ -14,5 +14,5 @@ test_that("raw noise can produce red noise", {
   rerun(.n = 1000,
         raw_noise(timesteps = 100, mu = 0.5, sigma = 0.2, phi = 0.5)) %>%
     map_dbl(autocorrelation) %>% mean() -> test_red
-  expect_true(0.55 < test_red && test_red < 0.45)
+  expect_true(0.55 > test_red && test_red > 0.45)
 })
--- tests/testthat/test_timeseries.R
+++ tests/testthat/test_timeseries.R
@@ -1,13 +1,12 @@
-library(colorednoise)
 library(purrr)
 library(dplyr)
 context("Autocorrelation of timeseries output")
 
 test_that("timeseries can produce blue noise populations", {
   rerun(.n = 1000,
-        timeseries(start = 20, timesteps = 10, phi = -0.4, survMean = 0.6,
-        survSd = 0.52, fecundMean = 0.3, fecundSd = 0.7)) %>%
-    map(~mutate(., est_surv = survivors/(growth - population),
+        timeseries(start = 200, timesteps = 50, phi = -0.4, survMean = 0.9,
+        survSd = 0.6, fecundMean = 0.3, fecundSd = 0.7)) %>%
+    map(~mutate(., est_surv = survivors/(population - growth),
                 est_fecund = newborns/survivors)) %>%
     map_dbl(~autocorrelation(.$est_surv)) -> test_blue
   expect_true(mean(test_blue) < -0.1)
@@ -15,9 +14,9 @@ test_that("timeseries can produce blue noise populations", {
 
 test_that("timeseries can produce red noise populations", {
   rerun(.n = 1000,
-        timeseries(start = 20, timesteps = 10, phi = 0.4, survMean = 0.6,
-                   survSd = 0.52, fecundMean = 0.3, fecundSd = 0.7)) %>%
-    map(~mutate(., est_surv = survivors/(growth - population),
+        timeseries(start = 200, timesteps = 50, phi = 0.4, survMean = 0.9,
+                   survSd = 0.6, fecundMean = 0.3, fecundSd = 0.7)) %>%
+    map(~mutate(., est_surv = survivors/(population - growth),
                 est_fecund = newborns/survivors)) %>%
     map_dbl(~autocorrelation(.$est_surv)) -> test_red
   expect_true(mean(test_red) > 0.1)
