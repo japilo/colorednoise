5c3273efadba7e3be4ffece0ebba8cee105bb20a
Julia Pilowsky <japilo@gmail.com>
2018-03-07 16:27:54 +0100

    Temporarily disabling demographic stochasticity, as the results are currently unreliable.

--- R/simulate_populations.R
+++ R/simulate_populations.R
@@ -38,13 +38,13 @@
 #'                                fecundMean = 1.1, fecundSd = 0.5, replicates = 50)
 #' head(survival_range[[1]])
 #' @export
-autocorr_sim <- function(timesteps, start, survPhi, fecundPhi, survMean, 
+autocorr_sim <- function(timesteps, start, survPhi, fecundPhi, survMean,
     survSd, fecundMean, fecundSd, replicates) {
     # Simulates a population for each combination of parameters
     # -----------
-    raw_sims <- cross_df(list(start = start, timesteps = timesteps, 
-        survPhi = survPhi, fecundPhi = fecundPhi, survMean = survMean, 
-        survSd = survSd, fecundMean = fecundMean, fecundSd = fecundSd)) %>% 
+    raw_sims <- cross_df(list(start = start, timesteps = timesteps,
+        survPhi = survPhi, fecundPhi = fecundPhi, survMean = survMean,
+        survSd = survSd, fecundMean = fecundMean, fecundSd = fecundSd)) %>%
         rerun(.n = replicates, pmap(., unstructured_pop))
     # Labels each simulation with the parameters that generated it
     # ------
@@ -52,12 +52,12 @@ autocorr_sim <- function(timesteps, start, survPhi, fecundPhi, survMean,
     for (i in 1:length(raw_sims)) {
         labeled_sims[[i]] <- vector(mode = "list", nrow(raw_sims[[1]][[1]]))
         for (j in 1:length(raw_sims[[i]][[2]])) {
-            labeled_sims[[i]][[j]] <- cbind(raw_sims[[i]][[2]][[j]], 
+            labeled_sims[[i]][[j]] <- cbind(raw_sims[[i]][[2]][[j]],
                 raw_sims[[i]][[1]][j, ])
         }
     }
     # Unnests the list and adds estimates of survival and fertility
-    sims <- labeled_sims %>% flatten() %>% map(~mutate(., est_surv = survivors/population, 
+    sims <- labeled_sims %>% flatten() %>% map(~mutate(., est_surv = survivors/population,
         est_fecund = newborns/survivors))
     return(sims)
 }
@@ -67,8 +67,7 @@ autocorr_sim <- function(timesteps, start, survPhi, fecundPhi, survMean,
 #' Simulate a structured population with temporal autocorrelation using standard Leslie matrices.
 #' Each element in the Leslie matrix has a specified mean, variance, and temporal autocorrelation value.
 #' The matrix can have arbitrary dimensions and can have transitions besides linear survival. This model
-#' includes environmental stochasticity with colored noise as well as demographic stochasticity. Density
-#' dependence is not currently supported.
+#' includes environmental stochasticity with colored noise. Density dependence and demographic stochasticity not currently supported.
 #'
 #' @param data The input data can be one of two formats: a list of three matrices, or a data frame
 #' with three columns. \cr
@@ -91,13 +90,6 @@ autocorr_sim <- function(timesteps, start, survPhi, fecundPhi, survMean,
 #' many plant matrices. If your matrix has transition probabilities in the first row or fecundities beyond the first row
 #' (e.g., clonal reproduction), provide a character matrix here with the same dimensions as your matrix that gives in
 #' strings whether each element is 'fecundity' or 'transition'.
-#' @param demoStochasticity By default, the matrix model is run without demographic stochasticity - that is,
-#' it simply matrix-multiplies the population vector by a stochastic matrix of vital rates. If you would prefer a
-#' stochastic projection with demographic stochasticity, that is, the given vital rates are used as rates for
-#' probability distributions of survival and fertility, set this argument to TRUE. Please note that the function becomes
-#' much more computationally intensive if you turn on demographic stochasticity. It is also important to note that the
-#' deomgraphic stochasticity helper function cannot handle population sizes larger than the maximum for 32-bit integers,
-#' and will therefore crash if asked to model large populations with high population growth for long time periods.
 #' @return A data frame with n + 2 columns, where n is the number of stages in the matrix. One column indicates the timestep,
 #' there is one column with the population size for each stage, and one column for total population size.
 #' @examples
@@ -108,14 +100,14 @@ autocorr_sim <- function(timesteps, start, survPhi, fecundPhi, survMean,
 #' sim <- matrix_model(list(meanMat, sdMat, phiMat), initialPop, 50)
 #' head(sim)
 #' @export
-matrix_model <- function(data, initialPop, timesteps, corrMatrix = NULL, 
-    colNames = NULL, matrixStructure = NULL, demoStochasticity = FALSE) {
+matrix_model <- function(data, initialPop, timesteps, corrMatrix = NULL,
+    colNames = NULL, matrixStructure = NULL) {
     stages <- length(initialPop)
     if (is.data.frame(data) == T) {
         if (is.null(colNames) == F) {
             data <- data[, colNames] %>% rename(!(!(!colNames)))
         }
-        if (all(names(data) == c("mean", "sd", "autocorrelation")) == 
+        if (all(names(data) == c("mean", "sd", "autocorrelation")) ==
             F) {
             stop("Please name data frame columns correctly")
         }
@@ -139,51 +131,43 @@ matrix_model <- function(data, initialPop, timesteps, corrMatrix = NULL,
         if (all(data[[2]] >= 0) == F) {
             stop("Invalid values in SD matrix")
         }
-        dat <- tibble(mean = as.vector(t(data[[1]])), sd = as.vector(t(data[[2]])), 
+        dat <- tibble(mean = as.vector(t(data[[1]])), sd = as.vector(t(data[[2]])),
             autocorrelation = as.vector(t(data[[3]])))
     } else {
         stop("Invalid data type. Must be a list of three matrices or a data frame with three columns.")
     }
     if (is.null(matrixStructure) == T) {
-        df <- dat %>% cbind(dist = c(rep("log", stages), rep("qlogis", 
+        df <- dat %>% cbind(dist = c(rep("log", stages), rep("qlogis",
             nrow(dat) - stages))) %>% mutate(dist = as.character(dist))
     } else if (all(dim(matrixStructure) == c(stages, stages)) == T) {
         stopifnot(matrixStructure == c("fecundity", "transition"))
-        dists <- ifelse(as.vector(t(matrixStructure)) == "fecundity", 
+        dists <- ifelse(as.vector(t(matrixStructure)) == "fecundity",
             "log", "qlogis")
         df <- dat %>% cbind(dist = dists) %>% mutate(dist = as.character(dist))
     } else {
         stop("Either your initial population vector has an invalid length or your matrixStructure is invalid")
     }
     if (is.null(corrMatrix) == T) {
-        elements <- df %>% rowwise() %>% mutate(mean.trans = ifelse(mean == 
-            0, 0, invoke(dist, list(mean))), sd.trans = ifelse(sd == 
-            0, 0, variancefix(mean, sd, dist)), noise = list(colored_noise(timesteps, 
-            mean.trans, sd.trans, autocorrelation)), natural.noise = ifelse(all(noise == 
-            0) == T, list(noise), ifelse(dist == "log", list(exp(noise)), 
+        elements <- df %>% rowwise() %>% mutate(mean.trans = ifelse(mean ==
+            0, 0, invoke(dist, list(mean))), sd.trans = ifelse(sd ==
+            0, 0, variancefix(mean, sd, dist)), noise = list(colored_noise(timesteps,
+            mean.trans, sd.trans, autocorrelation)), natural.noise = ifelse(all(noise ==
+            0) == T, list(noise), ifelse(dist == "log", list(exp(noise)),
             list(plogis(noise)))))
     } else if (is.matrix(corrMatrix) == T) {
-        elements <- df %>% rowwise() %>% mutate(mean.trans = invoke(dist, 
+        elements <- df %>% rowwise() %>% mutate(mean.trans = invoke(dist,
             list(mean)), sd.trans = variancefix(mean, sd, dist))
-        elements$noise <- colored_multi_rnorm(100, elements$mean.trans, 
-            elements$sd.trans, elements$autocorrelation, corrMatrix) %>% 
+        elements$noise <- colored_multi_rnorm(100, elements$mean.trans,
+            elements$sd.trans, elements$autocorrelation, corrMatrix) %>%
             split(rep(1:ncol(.), each = nrow(.)))
-        elements <- elements %>% mutate(natural.noise = ifelse(dist == 
+        elements <- elements %>% mutate(natural.noise = ifelse(dist ==
             "log", list(exp(noise)), list(plogis(noise))))
     } else {
         stop("Correlation matrix must be in matrix format")
     }
-    if (demoStochasticity == T) {
-        population <- demo_stochasticity(initialPop, elements$natural.noise)
-        population %>% as_tibble() %>% by_row(., sum, .collate = "cols", 
-            .to = "total") %>% mutate(timestep = 1:n()) %>% select(timestep, 
-            everything()) %>% set_names(c("timestep", paste0("stage", 
-            1:stages), "total"))
-    } else if (demoStochasticity == F) {
-        population <- projection(initialPop, elements$natural.noise)
-        population %>% map(as_tibble) %>% bind_rows() %>% by_row(., 
-            sum, .collate = "cols", .to = "total") %>% mutate(timestep = 1:n()) %>% 
-            select(timestep, everything()) %>% set_names(c("timestep", 
-            paste0("stage", 1:stages), "total"))
-    }
+      population <- projection(initialPop, elements$natural.noise)
+      population %>% map(as_tibble) %>% bind_rows() %>% by_row(.,
+          sum, .collate = "cols", .to = "total") %>% mutate(timestep = 1:n()) %>%
+          select(timestep, everything()) %>% set_names(c("timestep",
+          paste0("stage", 1:stages), "total"))
 }
